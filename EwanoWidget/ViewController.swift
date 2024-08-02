f4370f532c6af80605f21712306d33b23b93d1d4
Pooyan J <pooyanjavidi@gmail.com>
2024-08-02 17:53:03 +0330

    Initial Commit

---
+++ EwanoWidget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EwanoWidget
+//
+//  Created by Pooyan J on 5/12/1403 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
